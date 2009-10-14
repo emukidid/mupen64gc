c0e50fda9d625a1b39dd433d64e7dca7d6579b1d
tehpola <tehpola@users.noreply.github.com>
2009-10-14 12:11:00 +0000

    Reworked SQRT_FP to use less (and faster) instructions, and properly handle sqrt(0).

--- r4300/ppc/PowerPC.h
+++ r4300/ppc/PowerPC.h
@@ -984,6 +984,15 @@ PowerPC_instr Instruction(int opcode, ...);
 	  PPC_SET_RD    (ppc, (fd)); \
 	  PPC_SET_RB    (ppc, (fs)); }
 
+#define GEN_FSEL(ppc,fd,fa,fb,fc) \
+	{ ppc = NEW_PPC_INSTR(); \
+	  PPC_SET_OPCODE(ppc, PPC_OPCODE_FPD); \
+	  PPC_SET_FUNC  (ppc, PPC_FUNC_FSEL); \
+	  PPC_SET_RD    (ppc, (fd)); \
+	  PPC_SET_RA    (ppc, (fa)); \
+	  PPC_SET_RB    (ppc, (fb)); \
+	  PPC_SET_RC    (ppc, (fc)); }
+
 #define GEN_BCLR(ppc,lk,bo,bi) \
 	{ ppc = NEW_PPC_INSTR(); \
 	  PPC_SET_OPCODE(ppc, PPC_OPCODE_XL); \
