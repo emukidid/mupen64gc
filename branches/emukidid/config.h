d6d8bcef97902af5d71b0734e559294ab565edf8
emukidid <emukidid@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-10-20 12:27:33 +0000

    emukidid dir is now gone

--- branches/emukidid/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#undef WITH_HOME
-#undef VCR_SUPPORT
-#define GTK2_SUPPORT 1
-
-#endif /* CONFIG_H */
--- branches/emukidid/glN64_GX/Config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-void Config_LoadConfig();
-void Config_DoConfig();
-#endif
-
