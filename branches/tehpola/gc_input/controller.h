0d39814d64b8e50e2f9cd40f587c8a75d861ab86
tehpola <tehpola@693b9616-1639-0410-b3bc-ab0fbca43ce0>
2009-11-05 21:45:32 +0000

    Refactored gc_input to allow external code to unassign/assign controllers and to inspect the state of physical controllers

--- branches/tehpola/gc_input/controller.h
+++ branches/tehpola/gc_input/controller.h
@@ -31,4 +31,38 @@ typedef struct {
 	char available[4];
 } controller_t;
 
+typedef struct _virtualControllers_t {
+	BOOL          inUse;   // This virtual controller is being controlled
+	controller_t* control; // The type of controller being used
+	int           number;  // The physical controller number
+} virtualControllers_t;
+
+extern virtualControllers_t virtualControllers[4];
+
+// List of all the defined controller_t's
+#if defined(WII) && !defined(NO_BT)
+
+#define num_controller_t 2
+extern controller_t controller_GC;
+extern controller_t controller_Classic;
+controller_t* controller_ts[num_controller_t] =
+	{ &controller_GC, &controller_Classic,
+	 };
+int num_assigned[num_controller_t] = { 0, 0 };
+
+#else // WII && !NO_BT
+
+#define num_controller_t 1
+extern controller_t controller_GC;
+controller_t* controller_ts[num_controller_t] =
+	{ &controller_GC,
+	 };
+int num_assigned[num_controller_t] = { 0 };
+
+#endif // WII && !NO_BT
+
+void init_controller_ts(void);
+void assign_controller(int whichVirtual, controller_t*, int whichPhysical);
+void unassign_controller(int whichVirtual);
+
 #endif
